7b7d833f0dd85ddab1c61e0b72913b16b8d0b985
louish10 <louish10@hotmail.co.uk>
2021-02-04 10:11:52 +0000

    Use time cutoff for simulating the first network
    This is because the simulation should run for a specified amount of time, not for a set number of reactions. This gives an exact correspondence between the analytic and simulated data.

--- src/simulate_network_1.jl
+++ src/simulate_network_1.jl
@@ -1,46 +1,48 @@
 using Plots
 
 function main()
-    n_simulations = 50000
+    n_simulations = 500000
     steady_states = zeros(n_simulations)
     for i in 1:n_simulations
         x, t = simulate_network(5, 0.3)
-        steady_states[i] = last(x)
+        steady_states[i] = x
         println(i)
     end
     x_true = zeros(40)
     for i in 1:40
         x_true[i] = (1/factorial(big(i))*(5/0.3)^i * exp(-5/0.3))
     end
-    histogram(steady_states, nbins=21, normed=true)
+
+    nbins = Int(maximum(steady_states))
+    println(nbins)
+    histogram(steady_states, nbins=Int(nbins), normed=true)
     scatter!(1:40, x_true)
     xlabel!("x")
     ylabel!("P(x)")
-    savefig("network1.svg")
+    savefig("../network1.svg")
 end
 
-function simulate_network(k_1, k_2)
-    n_steps = 300
-    x = zeros(n_steps)
-    t = zeros(n_steps)
-    x[1] = rand( [0, 50], Int32)
-    t[1] = 0
 
-    for i in 2:n_steps
+function simulate_network(k_1, k_2)
+    t_final = 500
+    x = rand(1:50)
+    t = 0
+
+    while t < t_final
+        x_last = x
         prop_1 = k_1  
-        prop_2 = k_2 * x[i-1]
+        prop_2 = k_2 * x_last
         a = prop_1 + prop_2
 
         R = rand()
         dt = 1/a*log(1/(1-rand()))
         if R < prop_1/a
-            x[i] = x[i-1] + 1.
+            x = x_last + 1.
         else
-            x[i] = x[i-1] - 1.
+            x = x_last - 1.
         end
-        t[i] = t[i-1] + dt
+        t = t + dt
     end
     return x, t
 end
-
 main()
